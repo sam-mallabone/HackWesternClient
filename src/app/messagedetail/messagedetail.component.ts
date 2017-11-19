bc68bfcfca6af869f0468c4446251b5a80c37040
Sam Mallabone <smallabo@uwo.ca>
2017-11-19 03:27:26 -0500

    Adding the new stuff

--- src/app/messagedetail/messagedetail.component.ts
+++ src/app/messagedetail/messagedetail.component.ts
@@ -14,7 +14,13 @@ const httpOptions = {
 export class MessagedetailComponent implements OnInit {
   private url = "https://hackwesternserver-sammallabone.c9users.io/api";
   //variable that is bound to the View
-  message;
+  //message;
+  public myMessage;
+  public username;
+  public message; 
+  public time; 
+  sentiment; anger; joy; surprise;
+  sadness; fear;
   constructor(private location: Location, 
     private http: HttpClient, 
     private route: ActivatedRoute) { }
@@ -26,7 +32,31 @@ export class MessagedetailComponent implements OnInit {
         .subscribe(data => {
           console.log(data);
               //All these objects exist they are just not seen
-              this.message = data;
+              //Says there are errors but there actually isn't. It just can't 
+              //this.message = data;
+              this.username = data.username;
+              this.message = data.message;
+              
+              var timeNow = Date.now() / 1000;
+              var oldTime = +data.time;
+              oldTime = oldTime / 1000;
+              var timeDiff = Math.round(timeNow - oldTime);
+              if(timeDiff < 60){
+                this.time = timeDiff.toString() + " seconds ago";
+              }
+              else if(timeDiff < 3600){
+                this.time = Math.round((timeDiff / 60)).toString() + " minutes ago";
+              }
+              else if(timeDiff < 86400){
+                this.time = Math.round((timeDiff / 3600)).toString() + " hours ago"
+              }
+              this.sentiment= parseFloat(data.sentiment) * 100;
+              this.anger = parseFloat(data.anger) * 100;
+              this.joy = parseFloat(data.joy) * 100;
+              this.surprise = parseFloat(data.surprise) * 100;
+              this.sadness = parseFloat(data.sadness) * 100;
+              this.fear = parseFloat(data.fear) * 100;
+              this.myMessage = data;
         });
   }
 
